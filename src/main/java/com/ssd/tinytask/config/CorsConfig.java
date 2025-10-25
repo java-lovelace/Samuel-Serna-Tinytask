6b2f51e8bebba3d13a6622327e36dbe00f40dcd7
Sam-SSD <samuserna2005@gmail.com>
2025-10-25 18:31:58 -0500

    Fix: Update CORS configuration to allow requests from the production URL

--- src/main/java/com/ssd/tinytask/config/CorsConfig.java
+++ src/main/java/com/ssd/tinytask/config/CorsConfig.java
@@ -23,7 +23,8 @@ public class CorsConfig {
                                 "http://localhost:3000",
                                 "http://127.0.0.1:5500",
                                 "http://127.0.0.1:5501",
-                                "http://127.0.0.1:3000"
+                                "http://127.0.0.1:3000",
+                                "https://tinytask.up.railway.app"
                         )
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
